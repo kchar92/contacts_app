29fadcd63277d928f3885d044454915f7db7f20e
Keelana Char <keelanachar@gmail.com>
2019-11-10 10:51:04 -0600

    create contact show action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -6,4 +6,12 @@ class Api::ContactsController < ApplicationController
     render 'contacts_index.json.jb'
   end
 
+  def show
+
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+
+    render 'contact_index.json.jb'
+  end
+
 end
