6870c184e14a35367003d29180bcdad33ae37550
Keelana Char <keelanachar@gmail.com>
2019-11-12 18:50:13 -0600

    add middle name attribute to model. implemented it in the views file

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,22 +1,19 @@
 class Api::ContactsController < ApplicationController
-  
   def index
     @contacts = Contact.all
 
     render 'contacts_index.json.jb'
   end
-
   def show
-
     contact_id = params[:id]
     @contact = Contact.find_by(id: contact_id)
 
     render 'contact_index.json.jb'
   end
-
   def create
     @contact = Contact.new(
       first_name: params[:first_name],
+      middle_name: params[:middle_name],
       last_name: params[:last_name],
       phone_number: params[:phone_number],
       email: params[:email]
@@ -24,13 +21,12 @@ class Api::ContactsController < ApplicationController
     @contact.save
     render 'contact_index.json.jb'
   end
-
   def update
-
     contact_id = params[:id]
     @contact = Contact.find_by(id: contact_id)
 
     @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.email = params[:email] || @contact.email
@@ -39,11 +35,9 @@ class Api::ContactsController < ApplicationController
 
     render 'contact_index.json.jb'
   end
-
   def destroy
     @contact = Contact.find_by(id: params[:id])
     @contact.destroy
-
     render 'contact_destroy.json.jb'
   end
 
