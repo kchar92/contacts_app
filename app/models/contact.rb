6e54274c018bffe79425c64a9babd02130c198ab
Keelana Char <keelanachar@gmail.com>
2019-11-21 20:45:14 -0600

    create group model and join model, has_many through relationship

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   belongs_to :user
+  has_many :contact_groups
+  has_many :groups, through: :contact_groups
 
   def friendly_updated_at
     updated_at.strftime("%A, %b %d")
